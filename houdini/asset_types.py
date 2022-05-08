ab7485c8a2dce49df4bcf5f05beb4314bb9b08e2
Mehul Joshi <dashingmehul16@gmail.com>
2022-05-08 19:31:33 +0530

    Adding shot importer and hip file loader.

---
+++ houdini/asset_types.py
@@ -0,0 +1,22 @@
+ASSET_TYPES = {
+    "geometry": {
+        "main": "geo",
+        "child": "alembic",
+        "parm": "fileName"
+    },
+    "camera": {
+        "main": "alembicarchive",
+        "child": "null",
+        "parm": "fileName"
+    },
+    "fbx": {
+        "main": "geo",
+        "child": "file",
+        "parm": "file"
+    },
+    "vdb": {
+        "main": "geo",
+        "child": "file",
+        "parm": "file"
+    },
+}
--- houdini/importers.py
+++ houdini/importers.py
@@ -3,18 +3,40 @@ Houdini Importers.
 """
 # local imports
 import hou  # pylint: disable=import-error
+from asset_types import ASSET_TYPES
 
 
-def import_asset_shots(file_path="", name=None, **kwargs):
+def import_asset_shots(file_path="", name=None, asset_type=None, **kwargs):
     """
     Import Assets/Shot Object in the scene.
 
     params:
         file_path (str): File path to import.
-        file_type (str): File type to import in the scene.
         name (str): Asset name.
+        asset_type (str): Asset format type.
         **kwargs: Key word arguments.
     """
     obj = hou.node('/obj/')
-    node = obj.createNode('geo', name)
-    alembic = hou.node(node.path() + '/alembic1/').parm('fileName').set(file_path)
+    asset_data = ASSET_TYPES[asset_type]
+
+    node_obj = obj.createNode(asset_data["main"], name)
+
+    if asset_type == 'camera':
+        node_obj.parm(asset_data["parm"]).set(file_path)
+        node_obj.parm('buildHierarchy').pressButton()
+    else:
+        node = node_obj.createNode(asset_data["child"])
+        node.parm(asset_data["parm"]).set(file_path)
+
+
+def import_hip_file(file_path=None):
+    """
+    Import/Load hip file in current session.
+
+    params:
+        file_path (str): HIP file path.
+
+    returns:
+        None
+    """
+    hou.hipFile.merge(file_path)
